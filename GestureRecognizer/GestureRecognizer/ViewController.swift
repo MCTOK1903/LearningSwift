b15431f9384ea958ccbd6ff6022631bd927a60f1
MCT <celaltok2@gmail.com>
2020-03-08 00:09:58 +0300

    project completed

--- GestureRecognizer/GestureRecognizer/ViewController.swift
+++ GestureRecognizer/GestureRecognizer/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var rAndMimageView: UIImageView!
+    @IBOutlet weak var textLabel: UILabel!
+    var isChange = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        rAndMimageView.isUserInteractionEnabled = true
+        
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(changePic))
+        
+        rAndMimageView.addGestureRecognizer(gestureRecognizer)
+        
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @objc func changePic(){
+        
+        if isChange == false{
+            rAndMimageView.image = UIImage(named: "rickandMorty")
+            textLabel.text = "rickAndMorty1"
+            isChange=true
+        }else{
+            rAndMimageView.image = UIImage(named: "rickAndMorty2")
+            textLabel.text = "rickAndMorty2"
+            isChange=false
+        }
+    }
 }
 
