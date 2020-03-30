35781a89364f825aed2b9218e8bafcbff3387325
MCT <celaltok2@gmail.com>
2020-03-30 16:30:07 +0300

    pod files of firebase added

---
+++ SnapchatCloneWithFirebae/SnapchatCloneWithFirebae/SignInVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnapchatCloneWithFirebae
+//
+//  Created by MCT on 29.03.2020.
+//  Copyright © 2020 MCT. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
